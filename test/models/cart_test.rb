c02729baa56864878ec9fe90f977a6cc69920028
zybouric <hoang_christine@yahoo.fr>
2018-11-26 23:49:38 +0100

    create association in tab cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+   test "associations" do
+    should belong_to :user
+  end
+  
 end
