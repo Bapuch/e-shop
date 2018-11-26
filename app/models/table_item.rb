da5291713b93cf565367d1e20ae6bee47482c284
zybouric <hoang_christine@yahoo.fr>
2018-11-26 14:29:58 +0100

    add tests

--- app/models/table_item.rb
+++ app/models/table_item.rb
@@ -1,2 +1,6 @@
 class TableItem < ApplicationRecord
+# title mandatory + uniqueness of the title during the creation of the product
+	validates :title, presence: true, uniqueness: true
+	validates :price, numericality: true
 end
+
--- test/models/table_item_test.rb
+++ test/models/table_item_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class TableItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @item = TableItem.new(title: "Example Item")
+  end
+#test to find if the item titele is uniq
+  test "title should be unique" do
+    duplicate_item = @item.dup
+    assert_not duplicate_item.valid?
+  end
+
+#  test "title should be unique" do
+#    duplicate_item = @item.dup
+#    @item.save(validate: false)
+#    duplicate_item.valid? # need this to populate errors
+#    assert duplicate_item.errors
+#    assert duplicate_item.errors[:title]
+#  end
+#test if item is save without title
+
+  test 'invalid without title' do
+    @item.title = nil
+    refute @item.valid?, 'saved item without a title'
+    assert_not_nil @item.errors[:title], 'no validation error for title present'
+  end
+
+
 end
