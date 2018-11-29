7677d1e4871eb81893c9f270df6359d0667418a1
Julien <gracia.julien@gmail.com>
2018-11-29 11:02:10 +0100

    methode get_total_price

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  # Return the total spent for a given user
+  def get_total_price(user)
+    sum = 0
+    user.orders.each do |order|
+      sum += order.total_price
+    end
+    return sum
+
+  end
 end
