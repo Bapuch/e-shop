a7a631e2a51916cee31d382d7ebf245038272543
papuch <ngm.etcetera@gmail.com>
2018-11-30 03:26:59 +0100

    update links for boutique on home page and navbar

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,6 +3,23 @@ class CategoryController < ApplicationController
     @categories = Category
     @active_cat = 0
     @all_items = Item
+    puts params
+    puts "ICI LA HY HO"
+
+    if params[:id].nil?
+      @active_cat = 0
+      @items = Item
+    else
+      @active_cat = params[:id]
+      @items = Item.where(category_id: @active_cat)
+    end
+
+  end
+
+  def category
+    @categories = Category
+    @active_cat = 0
+    @all_items = Item
     @items = Item
   end
 
