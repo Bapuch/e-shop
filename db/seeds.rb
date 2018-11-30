95a7404eb65a17fda8ce3c54084791b08e7557e9
papuch <ngm.etcetera@gmail.com>
2018-11-30 04:36:01 +0100

    seed database with war bots and add images

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Item.destroy_all
-Category.destroy_all
 
-
+def category
 # Create 3 categories
-
-Category.create(name: "Robots guerriers")
-Category.create(name: "Robots ménagers")
-Category.create(name: "Sexbot")
+  Category.create(name: "Robots guerriers")
+  Category.create(name: "Robots ménagers")
+  Category.create(name: "Sexbot")
+end
 # Create 20 random items
  #destroy all items first
-20.times do |i|
-  new_item = Item.new
-  new_item.title = Faker::Name.name
-  new_item.description  = Faker::Lorem.paragraph(8)
-  new_item.price = Faker::Number.decimal(2)
-  new_item.image_url = Faker::Avatar.image
-  # temporaly, add item to first category
-  new_item.category_id = Category.first.id
+def test_item
+  20.times do |i|
+    new_item = Item.new
+    new_item.title = Faker::Name.name
+    new_item.description  = Faker::Lorem.paragraph(8)
+    new_item.price = Faker::Number.decimal(2)
+    new_item.image_url = Faker::Avatar.image
+    # temporaly, add item to first category
+    new_item.category_id = Category.first.id
+    new_item.save
+  end
+end
+
+def warbot
+  cat = Category.find_by(name: "Robots guerriers")
+  new_item = Item.new(title: "Chewbacca", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_1_", category_id: cat.id)
+  new_item.save
+  new_item = Item.new(title: "Fighter", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_2_", category_id: cat.id)
+  new_item.save
+  new_item = Item.new(title: "Optimus Prime", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_3_", category_id: cat.id)
+  new_item.save
+  new_item = Item.new(title: "Army Robbot", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_4_", category_id: cat.id)
+  new_item.save
+  new_item = Item.new(title: "Terminator", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_5_", category_id: cat.id)
   new_item.save
 end
+
+def del_all_items
+  Item.destroy_all
+end
+
+warbot
