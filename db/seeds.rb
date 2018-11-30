47a1a7cceb0e4036cbf81c4b158147137b5b5945
papuch <ngm.etcetera@gmail.com>
2018-11-30 04:53:43 +0100

    update seed and views so correct pictures are displayed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 
 def category
 # Create 3 categories
-  Category.create(name: "Robots guerriers")
-  Category.create(name: "Robots ménagers")
-  Category.create(name: "Sexbot")
+  Category.create(name: "War Bots")
+  Category.create(name: "House Bots")
+  Category.create(name: "Sex Bots")
 end
 # Create 20 random items
  #destroy all items first
@@ -29,16 +29,26 @@ def test_item
 end
 
 def warbot
-  cat = Category.find_by(name: "Robots guerriers")
+  cat = Category.find_by(name: "War Bots")
   new_item = Item.new(title: "Chewbacca", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_1_", category_id: cat.id)
   new_item.save
   new_item = Item.new(title: "Fighter", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_2_", category_id: cat.id)
   new_item.save
-  new_item = Item.new(title: "Optimus Prime", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_3_", category_id: cat.id)
+  new_item = Item.new(title: "Optimus Prime", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(2), image_url: "warbot_3_", category_id: cat.id)
   new_item.save
-  new_item = Item.new(title: "Army Robbot", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_4_", category_id: cat.id)
+  new_item = Item.new(title: "Army Robbot", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(2), image_url: "warbot_4_", category_id: cat.id)
   new_item.save
   new_item = Item.new(title: "Terminator", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "warbot_5_", category_id: cat.id)
+  new_item.save
+end
+
+def sexbot
+  cat = Category.find_by(name: "Sex Bots")
+  new_item = Item.new(title: "Marcus Lee", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "sexbot_n1_", category_id: cat.id)
+  new_item.save
+  new_item = Item.new(title: "Denver", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(2), image_url: "sexbot_n2_", category_id: cat.id)
+  new_item.save
+  new_item = Item.new(title: "Kiki", description: Faker::Lorem.paragraph(8), price: Faker::Number.decimal(3), image_url: "sexbot_n3_", category_id: cat.id)
   new_item.save
 end
 
@@ -46,4 +56,4 @@ def del_all_items
   Item.destroy_all
 end
 
-warbot
+sexbot
